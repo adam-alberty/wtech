000fd413195695126f58d1f6f4b9d8387e89d852
adam-alberty <xalberty@stuba.sk>
2025-03-25 11:16:05 +0100

    add default title

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }} | @yield('title')</title>
+    <title>{{ config('app.name') }} | @yield('title', 'Step into Style – The Best Shoes for Every Occasion')</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
