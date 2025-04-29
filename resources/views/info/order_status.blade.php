9d625e1668785c7a6147b1dff6099cd32ed7cc92
adam-alberty <xalberty@stuba.sk>
2025-04-29 18:30:15 +0200

    update page styles

--- resources/views/info/order_status.blade.php
+++ resources/views/info/order_status.blade.php
@@ -1,25 +1,25 @@
 <x-layouts.default>
 
-    <div class="text-center mt-10 mb-10">
+    <div class="text-center pt-20 mb-10">
         <h2 class="text-3xl font-semibold">View Your Order</h2>
-        <h2 class="">To check the status of your order
-            please enter your order number and email address.</h2>
+        <p class="text-muted-foreground mt-3">To check the status of your order
+            please enter your order number and email address.</p>
     </div>
 
     <div class="max-w-md mx-auto mb-10">
-    <form action="{{ route('get_order_status') }}" method="POST" class="grid gap-2 mb-8">
-        @csrf
-        <input type="text" name="order_number" placeholder="Order number" class="p-3 rounded-xl border w-full">
-        <input type="email" name="email" placeholder="Email" class="p-3 rounded-xl border w-full">
+        <form action="{{ route('get_order_status') }}" method="POST" class="grid gap-2 mb-8">
+            @csrf
+            <input type="text" name="order_number" placeholder="Order number" class="p-3 rounded-xl border w-full">
+            <input type="email" name="email" placeholder="Email" class="p-3 rounded-xl border w-full">
 
-        <div class="flex justify-end mt-5">
-            <div class="shrink-0 flex">
-                <button type="submit"
+            <div class="flex justify-end mt-5">
+                <div class="shrink-0 flex">
+                    <button type="submit"
                         class="bg-primary py-3 px-5 rounded-full text-primary-foreground font-semibold">
-                    Submit</button>
+                        Check order</button>
+                </div>
             </div>
-        </div>
-    </form>
+        </form>
 
         @if (isset($order))
             <div class="max-w-md mx-auto bg-gray-100 p-5 rounded-xl text-center">
--- resources/views/info/shipping_and_delivery.blade.php
+++ resources/views/info/shipping_and_delivery.blade.php
@@ -1,38 +1,32 @@
 <x-layouts.default>
-    <div class="text-center mt-10">
-        <h2 class="text-3xl font-semibold">Shipping and Delivery</h2>
-    </div>
-    <div class="px-100">
-        <section class="w-full">
-            <div class="mx-auto mt-10 mb-10 text-xl line-height">
-                <h2 class="mb-5 ">
-                    At WTECH Shoes, we offer a variety of shipping methods to ensure that your order arrives
-                    in a timely and efficient manner. Choose from standard, expedited, and express shipping
-                    options during checkout.
-                </h2>
+    <div class="max-w-screen-lg mx-auto px-8 py-20">
+        <section class="prose prose-lg mx-auto prose-ul:list-outside">
+            <h2 class="text-center">Shipping and Delivery</h2>
+            <p class="mb-5 ">
+                At WTECH Shoes, we offer a variety of shipping methods to ensure that your order arrives
+                in a timely and efficient manner. Choose from standard, expedited, and express shipping
+                options during checkout.
+            </p>
 
-                <h5>Standard Shipping</h5>
-                <ul class="list-disc list-inside ml-5">
-                    <li>Delivery time: 5-7 business days</li>
-                    <li>Free on orders over €50</li>
-                </ul>
+            <h3>Standard Shipping</h3>
+            <ul class="list-disc list-inside ml-5">
+                <li>Delivery time: 5-7 business days</li>
+                <li>Free on orders over €50</li>
+            </ul>
 
-                <h5>Expedited Shipping</h5>
-                <ul class="list-disc list-inside ml-5">
-                    <li>Delivery time: 2-4 business days</li>
-                    <li>€5.99 for orders under $50</li>
-                </ul>
+            <h3>Expedited Shipping</h3>
+            <ul class="list-disc list-inside ml-5">
+                <li>Delivery time: 2-4 business days</li>
+                <li>€5.99 for orders under $50</li>
+            </ul>
 
-                <h5>Express Shipping</h5>
-                <ul class="list-disc list-inside ml-5">
-                    <li>Delivery time: 1-2 business days</li>
-                    <li>€15.99 for all orders</li>
-                </ul>
-            </div>
-            <div class="mx-auto w-full mt-10 mb-20 text-xl line-height">
-                <h2>If you have any questions regarding shipping or delivery,
-                    please don’t hesitate to reach out to our customer support team.</h2>
-            </div>
+            <h3>Express Shipping</h3>
+            <ul class="list-disc list-inside ml-5">
+                <li>Delivery time: 1-2 business days</li>
+                <li>€15.99 for all orders</li>
+            </ul>
+            <p>If you have any questions regarding shipping or delivery,
+                please don't hesitate to reach out to our customer support team.</p>
         </section>
     </div>
 </x-layouts.default>
