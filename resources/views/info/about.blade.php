785044a540a6b02d0edb561a805cf7f34a88fb64
adam-alberty <xalberty@stuba.sk>
2025-04-29 18:21:57 +0200

    change career styles

--- resources/views/info/about.blade.php
+++ resources/views/info/about.blade.php
@@ -1,30 +1,26 @@
 <x-layouts.default>
     <div class="max-w-screen-lg mx-auto px-8 py-20">
-        <div class="text-center mt-10">
-        </div>
-        <div>
-            <section class="prose prose-lg mx-auto">
-                <h2 class="text-center mb-10">About WTECH Shoes</h2>
-                <p>
-                    Welcome to WTECH Shoes, where innovation meets style.
-                    We are passionate about crafting high-quality footwear that empowers you to move with confidence
-                    and comfort.
-                </p>
-                <p>
-                    Founded with a vision to redefine performance and fashion, WTECH Shoes blends cutting-edge
-                    technology with sleek design.
-                    Whether you're hitting the track, exploring the outdoors, or stepping out in the city,
-                    our shoes are built to support your every step.
-                </p>
-                <p>
-                    At WTECH Shoes, we believe in sustainability, durability, and customer satisfaction.
-                    Our mission is to create footwear that not only looks good but also feels great,
-                    while keeping our planet in mind.
-                </p>
-                <p>
-                    Join us on this journey and experience footwear that’s designed to keep up with you.
-                </p>
-            </section>
-        </div>
+        <section class="prose prose-lg mx-auto">
+            <h2 class="text-center mb-10">About WTECH Shoes</h2>
+            <p>
+                Welcome to WTECH Shoes, where innovation meets style.
+                We are passionate about crafting high-quality footwear that empowers you to move with confidence
+                and comfort.
+            </p>
+            <p>
+                Founded with a vision to redefine performance and fashion, WTECH Shoes blends cutting-edge
+                technology with sleek design.
+                Whether you're hitting the track, exploring the outdoors, or stepping out in the city,
+                our shoes are built to support your every step.
+            </p>
+            <p>
+                At WTECH Shoes, we believe in sustainability, durability, and customer satisfaction.
+                Our mission is to create footwear that not only looks good but also feels great,
+                while keeping our planet in mind.
+            </p>
+            <p>
+                Join us on this journey and experience footwear that’s designed to keep up with you.
+            </p>
+        </section>
     </div>
 </x-layouts.default>
