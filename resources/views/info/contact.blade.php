0c13ec41c19965e8cbfb92d4f5c21107abd9a525
adam-alberty <xalberty@stuba.sk>
2025-04-29 18:18:10 +0200

    change contact styles

--- resources/views/info/contact.blade.php
+++ resources/views/info/contact.blade.php
@@ -1,22 +1,21 @@
 <x-layouts.default>
-    <div class="text-center mt-10 mb-15">
-        <h2 class="text-3xl font-semibold">Contact Us</h2>
+    <div class="max-w-screen-lg mx-auto grid gap-5 px-8 py-20">
+        <h2 class="text-3xl font-semibold text-center mb-5">Contact Us</h2>
+        <div class="bg-muted p-3 flex gap-5 items-start">
+            <x-phosphor-envelope class="w-8 h-8" />
+            <div>
+                <h3 class="font-semibold">Email Us</h3>
+                <p><a href="mailto:support@wtechshoes.com">support@wtechshoes.com</a></p>
+                <p class="text-muted-foreground mt-1">Available 24/7</p>
+            </div>
+        </div>
+        <div class="bg-muted p-3 flex gap-5 items-start">
+            <x-phosphor-phone class="w-8 h-8" />
+            <div>
+                <h3 class="font-semibold">Call Us</h3>
+                <p>+1 234 567 890</p>
+                <p class="text-muted-foreground mt-1">9:00 - 18:00, Monday - Friday</p>
+            </div>
+        </div>
     </div>
-
-    <section class="text-center grid grid-cols-2 mx-50 mb-20 line-height">
-        <div>
-            <i class="fas fa-envelope fa-3x mb-5"></i>
-            <h5 class="mt-2">Email Us</h5>
-            <p><a href="mailto:support@wtechshoes.com">support@wtechshoes.com</a></p>
-            <p>Available 24/7</p>
-        </div>
-        <div>
-            <i class="fas fa-phone fa-3x mb-5"></i>
-            <h5 class="mt-2">Call Us</h5>
-            <p>+1 234 567 890</p>
-            <p>9:00 - 18:00, Monday - Friday</p>
-        </div>
-    </section>
-
-
 </x-layouts.default>
